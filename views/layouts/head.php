f5d2433c2b205bbb1eea636e164bbcf88a9f6998
Chris Tembreull <chris@tembreull.org>
2015-04-14 16:20:16 -0700

    re-integrate flyout menu

--- views/layouts/head.php
+++ views/layouts/head.php
@@ -1,6 +1,10 @@
 <?php $ver = HVersion::VERSION; ?>
 
 <!-- start: CSS -->
+<link href="<?php echo Yii::app()->baseUrl; ?>/css/jasny-bootstrap.min.css" rel="stylesheet">
+<script type="text/javascript"
+        src="<?php echo Yii::app()->baseUrl; ?>/js/jasny-bootstrap.js"></script>
+
 <link href="<?php echo Yii::app()->theme->baseUrl; ?>/css/datepicker.css?ver=<?php echo $ver; ?>" rel="stylesheet">
 <link href="<?php echo Yii::app()->theme->baseUrl; ?>/css/theme.css?ver=<?php echo $ver; ?>" rel="stylesheet">
 <link href="<?php echo Yii::app()->theme->baseUrl; ?>/css/flatelements.css?ver=<?php echo $ver; ?>" rel="stylesheet">
